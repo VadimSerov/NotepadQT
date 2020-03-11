1760f0aa4772338f839490dcd4357d6e65956d3d
Серов Вадим Владимирович <v.v.serov@utmn.ru>
2020-03-11 16:12:06 +0500

    fix Icons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,3 +14,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,"Open the file");
+}
